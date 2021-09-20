ea3ef089e75fb5fa797509a9b53fc31c986c1c9e
Yugenn <yuruseajyan@gmail.com>
2021-09-20 22:53:22 +0900

    惑星情報管理アプリCRUD機能作成

--- app/Http/Controllers/PlanetController.php
+++ app/Http/Controllers/PlanetController.php
@@ -3,11 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Models\Planet;
-use Illuminate\Http\Request;
+use Database\Seeders\PlanetSeeder;
+use App\Http\Requests\PlanetRequest;
 
 class PlanetController extends Controller
 {       // showページへ移動
-       // indexページへ移動
+    // indexページへ移動
     public function index()
     {
         // モデル名::テーブル全件取得
@@ -22,12 +23,43 @@ class PlanetController extends Controller
         return view('planets.show', ['planet' => $planet]);
     }
 
-    public function create()
+    public function edit($id)
     {
-        return view('planets.create');
+        $planet = Planet::find($id);
+        return view('planets.edit', ['planet' => $planet]);
     }
 
-        public function store(Request $request)
+    public function update(PlanetRequest $request, $id)
+    {
+        // ここはidで探して持ってくる以外はstoreと同じ
+        $planet = Planet::find($id);
+
+        // 値の用意
+        $planet->name = $request->name;
+        $planet->en_name = $request->en_name;
+        $planet->radius = $request->radius;
+        $planet->weight = $request->weight;
+
+        // 保存
+        $planet->save();
+
+        // 登録したらindexに戻る
+        return redirect('/planets');
+    }
+
+    public function destroy($id)
+    {
+        $planet = Planet::find($id);
+        $planet->delete();
+        return redirect('/planets');
+    }
+
+    public function create()
+    {
+        return view('/planets.create');
+    }
+
+    public function store(PlanetRequest $request)
     {
         // インスタンスの作成
         $planet = new Planet;
--- resources/views/planets/edit.blade.php
+++ resources/views/planets/edit.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Planet create</title>
+    <title>Planet edit</title>
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
 
@@ -22,28 +22,30 @@
             </ul>
         </div>
     @endif
-    <h1>惑星情報登録</h1>
-    <form action="/planets" method="post">
+    <h1>惑星情報編集</h1>
+    <form action="/planets/{{ $planet->id }}" method="post">
         @csrf
+        @method('PATCH')
+        <input type="hidden" name="id" value="{{ $planet->id }}">
         <p>
             <label for="name">名前</label>
-            <input type="text" name="name" value="{{ old('name') }}">
+            <input type="text" name="name" value="{{ old('name', $planet->name) }}">
         </p>
         <p>
             <label for="description">名前(英語)</label>
-            <input type="text" name="en_name" value="{{ old('en_name') }}">
+            <input type="text" name="en_name" value="{{ old('en_name', $planet->en_name) }}">
         </p>
         <p>
             <label for="price">半径</label>
-            <input type="number" name="radius" value="{{ old('radius') }}">
+            <input type="number" name="radius" value="{{ old('radius', $planet->radius) }}">
         </p>
         <p>
             <label for="seller">重量</label>
-            <input type="number" name="weight" value="{{ old('weight') }}">
+            <input type="number" name="weight" value="{{ old('weight', $planet->weight) }}">
         </p>
 
 
-        <input type="submit" value="登録">
+        <input type="submit" value="更新">
     </form>
     <a href="/planets">戻る</a>
 </body>
